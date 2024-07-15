9ede7d925c9d06ede4eecbb0e9a3d158b2f2340b
VladimirMedv <167883925+VladimirMedv@users.noreply.github.com>
2024-07-15 11:56:29 -0300

    added user and logout button

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,6 +10,7 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
+import { LogOut } from "lucide-react";
 
 interface SidebarProps {
   isCollapsed: boolean;
@@ -76,6 +77,28 @@ function Sidebar({ isCollapsed }: SidebarProps) {
           )
         )}
       </ScrollArea>
+
+      {/* logout section */}
+      <div className="mt-auto">
+        <div className="flex justify-between items-center gap-2 md:px-6 py-2">
+          {!isCollapsed && (
+            <div className="hidden md:flex gap-2 items-center">
+              <Avatar className="my-1 flex justify-center items-center">
+                <AvatarImage
+                  src={"/user-placeholder.png"}
+                  alt="avatar"
+                  referrerPolicy="no-referrer"
+                  className="w-8 h-8 border-2 border-white rounded-full"
+                />
+              </Avatar>
+              <p className="font-bold">{"John Doe"}</p>
+            </div>
+          )}
+          <div className="flex">
+            <LogOut size={22} cursor={"pointer"} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
